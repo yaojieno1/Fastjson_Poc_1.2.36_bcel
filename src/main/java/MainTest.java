7907fa3ae18f892188ec7aace56cc7e01552ea24
yao jie <yaojieno1@hotmail.com>
2020-10-27 21:38:38 +0800

    增加Fastjson 37版本 bcel poc

--- src/main/java/MainTest.java
+++ src/main/java/MainTest.java
@@ -6,11 +6,19 @@ import java.nio.file.Files;
 public class MainTest {
 
     public static void main(String[] args) throws Exception {
-        String  filename    = System.getProperty("user.dir") + "/src/main/resources/poc-bcel-36-parseobject.txt";
+        String  filename    = System.getProperty("user.dir") + "/src/main/resources/poc-bcel-37-parseobject.txt";
         String poc = new String(Files.readAllBytes((new File(filename)).toPath() ));
 
         System.out.println(poc);
-        JSON.parseObject(poc, User.class);
+
+        //case 1, ok
+//        JSON.parse(poc);
+
+        //case 2, ok
+        JSON.parseObject(poc);
+
+        //case 3, no ok
+        //JSON.parseObject(poc, User.class);
     }
 }
 
